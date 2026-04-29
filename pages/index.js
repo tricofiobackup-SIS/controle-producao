26ea590604866c442dcc31b59042caa7ecde7eea
tricofiobackup-SIS <tricofiobackup@gmail.com>
2026-04-29 15:22:58 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Controle Produção</h1>
+      <p>Sistema em construção 🚀</p>
+    </div>
+  );
+}
