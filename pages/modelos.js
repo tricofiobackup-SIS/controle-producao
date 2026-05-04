628c94f2cdd2cb294b55c730143cdfc623b175dc
tricofiobackup-SIS <tricofiobackup@gmail.com>
2026-05-04 09:11:11 -0300

    Update modelos.js

--- pages/modelos.js
+++ pages/modelos.js
@@ -1,1 +1,11 @@
 
+import Layout from "../components/Layout";
+
+export default function Modelos() {
+  return (
+    <Layout>
+      <h1>Ficha de Modelos</h1>
+      <p>Cadastro completo dos modelos</p>
+    </Layout>
+  );
+}
