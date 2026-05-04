5932188712226466ec2c7274d2bed813208cec93
tricofiobackup-SIS <tricofiobackup@gmail.com>
2026-05-04 09:09:35 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,20 @@
 
+import Sidebar from "./Sidebar";
+
+export default function Layout({ children }) {
+  return (
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <main style={styles.main}>{children}</main>
+    </div>
+  );
+}
+
+const styles = {
+  main: {
+    flex: 1,
+    padding: 30,
+    background: "#AEBCC3",
+    minHeight: "100vh"
+  }
+};
