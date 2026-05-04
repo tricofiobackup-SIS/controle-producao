59fc0b5776bb1f20d9ba2916bc5f5626b91a9982
tricofiobackup-SIS <tricofiobackup@gmail.com>
2026-05-04 09:10:50 -0300

    Update cadastro-geral.js

--- pages/cadastro-geral.js
+++ pages/cadastro-geral.js
@@ -1,1 +1,11 @@
 
+import Layout from "../components/Layout";
+
+export default function CadastroGeral() {
+  return (
+    <Layout>
+      <h1>Cadastro Geral</h1>
+      <p>Aqui ficarão as listas do sistema</p>
+    </Layout>
+  );
+}
