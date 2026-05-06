7b54d60ad96658d9edc65677e3aa06cc19d27b15
tricofiobackup-SIS <tricofiobackup@gmail.com>
2026-05-06 16:14:56 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,46 @@
-import Home from "./home.js";
+import Layout from "../components/Layout";
 
-document.getElementById("app").innerHTML = Home();
+export default function Home() {
+  return (
+    <Layout>
+      <div className="cards">
+
+        <div className="card">
+          <h3>Bem-vindo ao Tricofio ERP</h3>
+
+          <p>
+            Sistema de gestão interno da Tricofio.
+          </p>
+        </div>
+
+        <div className="card">
+          <h3>Cadastro</h3>
+
+          <p>
+            Acesse modelos, combinações,
+            composições e produtos.
+          </p>
+        </div>
+
+        <div className="card">
+          <h3>O.S</h3>
+
+          <p>
+            Controle de ordens de serviço
+            e produção.
+          </p>
+        </div>
+
+        <div className="card">
+          <h3>Fios</h3>
+
+          <p>
+            Movimentações, entradas,
+            saídas e estoque.
+          </p>
+        </div>
+
+      </div>
+    </Layout>
+  );
+}
