63474441f25ed7f306b4ce8a27619351ef9f6195
tricofiobackup-SIS <tricofiobackup@gmail.com>
2026-05-06 11:29:23 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,31 @@
+import Sidebar from "./Sidebar.js";
+
 export default function Layout({ title, icon, breadcrumb, content }) {
   return `
-    <div class="page fade-in">
+    <div class="app-layout">
 
-      <div class="header">
-        <div class="header-left">
-          <i class="${icon}"></i>
-          <h1>${title}</h1>
+      ${Sidebar()}
+
+      <main class="main-content">
+        <div class="page fade-in">
+
+          <div class="header">
+            <div class="header-left">
+              <i class="${icon}"></i>
+              <h1>${title}</h1>
+            </div>
+
+            <div class="breadcrumb">
+              ${breadcrumb.join(" > ")}
+            </div>
+          </div>
+
+          <div class="content">
+            ${content}
+          </div>
+
         </div>
-
-        <div class="breadcrumb">
-          ${breadcrumb.join(" > ")}
-        </div>
-      </div>
-
-      <div class="content">
-        ${content}
-      </div>
+      </main>
 
     </div>
   `;
